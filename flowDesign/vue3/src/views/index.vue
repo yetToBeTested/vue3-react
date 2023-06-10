8dc11f2934d51e5b36792b67065705c00774fa61
wuwuwu <2624086659@qq.com>
2023-06-10 11:05:19 +0800

    test-vue3

---
+++ flowDesign/vue3/src/views/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="index">
+    <h2>index</h2>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="less">
+.index {
+  color: red;
+}
+</style>
